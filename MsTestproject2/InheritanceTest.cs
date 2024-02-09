524a877155801ff872ab3116a9ceef8f808ce2cb
Lokeshgithubcodes <ramisettylokesh642@gmail.com>
2024-02-09 20:37:05 +0530

    first commit

---
+++ MsTestproject2/InheritanceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsTestproject2
+{
+    public class InheritanceTest
+    {
+        public virtual string start()
+        {
+            return "Vehicle started";
+        }
+    }
+
+    public class Bike:InheritanceTest
+    {
+        public override string start()
+        {
+            return "Kick start";
+        }
+    }
+}
---
+++ MsTestproject2Tests/InheritanceTestTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MsTestproject2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsTestproject2.Tests
+{
+    [TestClass()]
+    public class InheritanceTestTests
+    {
+        [TestMethod()]
+        public void startTest()
+        {
+            InheritanceTest i=new InheritanceTest();
+            string s = i.start();
+            Assert.AreEqual("Vehicle started", s);
+            
+        }
+        [TestMethod()]
+
+        public void childMethod()
+        {
+            Bike b=new Bike();
+            string s=b.start();
+            Assert.AreEqual("Kick start", s);
+        }
+    }
+}
